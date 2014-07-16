b66a97e3cd9029714cb967d74540d423d295f824
Rodrigo <rodfcast@espol.edu.ec>
2014-07-16 09:43:30 -0500

    Se agrego la fecha a la vista de matriculacion

--- VistaMatriculacion.py
+++ VistaMatriculacion.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 import sys
 import time
 import threading
@@ -76,42 +77,50 @@ class VistaAgregarEstudiante(QWidget):
 		self.tab_uno.addTab(self.madre,"Madre")
 		self.tab_uno.addTab(self.representante,"Representante")
 		self.contenedor.addWidget(self.tab_uno)
-		#self.initEstudiante()
+		self.initEstudiante()
 		self.setLayout(self.contenedor)
 
 	def initEstudiante(self):
-		self.layout_estudiante=QFormLayout()
-		self.listaLayouts=[]
-		for i in range(7):
-			self.listaLayouts.append(QHBoxLayout())
+		self.layout_estudiante=QVBoxLayout()
+		self.listaLayouts=[QFormLayout(),QFormLayout()]
+		
+		#for i in range(8):
+		#	self.listaLayouts.append(QFormLayout())
 
-		listDatosEst = ["Nombres:","Apellidos:","Cedula:", "Sexo:","Estado Civil:","Origen:","Etnia:" ,"Fecha de nacimiento:"]
+		listDatosEst = ["Nombres:","Apellidos:",u"Cédula:", "Sexo:","Estado Civil:","Origen:","Etnia:" ,"Fecha de nacimiento:"]
 		indiceSexo=3
 		indiceEstadoCivil=4
-
-		for i in range(7):
-			self.listaLayouts[i].addWidget(QLabel(listDatosEst[i]))
-			if (i!=indiceSexo)and(i!=indiceEstadoCivil):
-				self.listaLayouts[i].addWidget(QLineEdit())
-			self.layout_estudiante.addRow(self.listaLayouts[i])
+		for i in range(0,3):
+			self.listaLayouts[0].addRow(QLabel(listDatosEst[i]),QLineEdit())
 
 		self.comboSexo=QComboBox()
-		self.sexos=["Masculino","Femenino"]
+		self.sexos=["MASCULINO","FEMENINO"]
 		self.comboSexo.addItems(self.sexos)
-		self.listaLayouts[indiceSexo].addWidget(self.comboSexo)
-		self.listaLayouts[indiceSexo].addWidget(QLabel("				"))
-		self.listaLayouts[indiceSexo].addWidget(QLabel("				"))
-		self.listaLayouts[i].addWidget(QLabel(listDatosEst[i]))
-
 
 		self.comboEstadoCivil=QComboBox()
-		self.EstadoCivil=["Soltero"]
+		self.EstadoCivil=["SOLTERO(A)","CASADO(A)","DIVORCIADO(A)","VIUDO(A)","UNIDO(A)"]
 		self.comboEstadoCivil.addItems(self.EstadoCivil)
-		self.listaLayouts[indiceEstadoCivil].addWidget(self.comboEstadoCivil)
-		self.listaLayouts[indiceEstadoCivil].addWidget(QLabel("				"))
-		self.listaLayouts[indiceEstadoCivil].addWidget(QLabel("				"))
+
+		self.listaLayouts[0].addRow(QLabel(listDatosEst[indiceSexo]),self.comboSexo)
+		self.listaLayouts[0].addRow(QLabel(listDatosEst[indiceEstadoCivil]),self.comboEstadoCivil)
+		self.listaLayouts[0].addRow(QLabel(listDatosEst[5]),QLineEdit())
+
+		self.comboEtnia=QComboBox()
+		self.Etnia=["BLANCO","MESTIZO","AFROECUATORIANO","INDIGENA", "MONTUBIO","NEGRO","MULATO","OTROS"]
+		self.comboEtnia.addItems(self.Etnia)
+
+		self.listaLayouts[0].addRow(QLabel(listDatosEst[6]),self.comboEtnia)
+
+		self.calendario=QCalendarWidget()
+		self.calendario.setMinimumDate(QDate(1940,1,1))
+		self.calendario.setMaximumDate(QDate(2015,1,1))
+		self.calendario.setGridVisible(False)
+
+		self.listaLayouts[0].addRow(QLabel(listDatosEst[7]),self.calendario)
 
 
+
+		self.layout_estudiante.addLayout(self.listaLayouts[0])
 
 
 		self.estudiante.setLayout(self.layout_estudiante)
