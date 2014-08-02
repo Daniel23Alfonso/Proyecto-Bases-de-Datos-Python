bf4061e6fd053a7334f89c80efc7f19774df5a6c
jorge Vergara <jorenver@espol.edu.ec>
2014-08-02 18:07:14 -0500

    se agrega la clase ManejadorBD

---
+++ ManejadorBD.py
@@ -0,0 +1,35 @@
+import MySQLdb
+
+class ManejadorBD():
+	def __init__(self):
+		self.direccion="127.0.0.1"
+		self.user="root"
+		self.passwor="sasukekun30"
+		self.nombreBD="BasePeliculas"
+
+
+	def conectar(self):
+		self.BD = MySQLdb.connect(self.direccion,self.user,self.passwor,self.nombreBD )
+
+
+	def desconectar(self):
+		self.BD.close()
+
+
+	def imprimirTablas(self):
+		self.conectar()
+		cursor=self.BD.cursor()
+		cursor.execute("SELECT VERSION()")
+		data = cursor.fetchone()
+		print "Version Base de Datos : %s " % data
+		print "Se conecto correctamente" 
+		cursor.execute("USE BasePeliculas")
+		cursor.execute("SHOW TABLES")
+		tables = cursor.fetchall() 
+		cont=0
+		for (table_name,) in cursor:
+			cont+=1
+			print(table_name)
+		print "numero de tablas: %d" %cont
+		self.desconectar()
+
--- Principal.py
+++ Principal.py
@@ -7,32 +7,10 @@ import threading
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from VistaIngresoSist import*
- 
-bd = MySQLdb.connect("127.0.0.1","root","sasukekun30","BasePeliculas" )
- 
-# Preparamos el cursor que nos va a ayudar a realizar las operaciones con la base de datos
-cursor = bd.cursor()
+from ManejadorBD import *
 
-cursor.execute("SELECT VERSION()")
-data = cursor.fetchone()
-print "Version Base de Datos : %s " % data
-print "Se conecto correctamente" 
-
-
-cursor.execute("USE BasePeliculas")
-cursor.execute("SHOW TABLES")
-tables = cursor.fetchall() 
-cont=0
-for (table_name,) in cursor:
-        cont+=1
-        print(table_name)
-print "numero de tablas: %d" %cont
- 
-
-
-
- 
-bd.close()
+M=ManejadorBD()
+M.imprimirTablas()
 app = QApplication(sys.argv)
 vista1 = VistaIngresoSist()
 vista1.show()
